5416f5acb492a05125e3562a3cbe3346e25fb1df
Mick <bmikdaam@gmail.com>
2024-01-31 19:58:44 +0100

    Add portfolio page with project data

---
+++ src/routes/portfolio/+page.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+  const { projects } = data;
+</script>
+
+<article class="active" data-page="blog">
+  <header>
+    <h2 class="h2 article-title">Projets</h2>
+  </header>
+
+  <section class="blog-posts">
+    <ul class="blog-posts-list">
+      {#each projects as project (project.link)}
+        <li class="blog-post-item">
+          <a href={project.link}>
+            <figure class="blog-banner-box">
+              <img src={project.image} alt={project.title} loading="lazy" />
+            </figure>
+
+            <div class="blog-content">
+              <div class="blog-meta">
+                {#each project.categories as category (category)}
+                  <p class="blog-category">{category}</p>
+                  {#if category !== project.categories[project.categories.length - 1]}
+                    <span class="dot"></span>
+                  {/if}
+                {/each}
+              </div>
+
+              <h3 class="h3 blog-item-title">{project.title}</h3>
+
+              <p class="blog-text">{project.description}</p>
+            </div>
+          </a>
+        </li>
+      {/each}
+    </ul>
+  </section>
+</article>
---
+++ src/routes/portfolio/+page.ts
@@ -0,0 +1,34 @@
+import type { PageLoad } from './$types';
+import ugpPic from "$lib/assets/projet-ugp.png";
+import mobistoryPic from "$lib/assets/projet-mobistory.png";
+import galaxyPic from "$lib/assets/projet-galaxy.png";
+
+const projects = [
+    {
+        link: "https://github.com/Mikdaam/Protocol-UGP",
+        image: ugpPic,
+        categories: ["Java", "Git", "Maven", "Réseaux"],
+        title: "Protocol UGP",
+        description: "Le protocole UGEGreed, développé pour le cours de 'Programmation Réseaux', vise à créer un protocole de distribution de calcul, 'UGP', pour tester efficacement des conjectures."
+    },
+    {
+        link: "https://github.com/Mikdaam/mobistory-flutter",
+        image: mobistoryPic,
+        categories: ["Flutter", "Sqlite", "Git"],
+        title: "Mobistory",
+        description: "Application mobile d'exploration des évènements historiques à proximité de l'utilisateur. Réalisé avec le framework Flutter."
+    },
+    {
+        link: "https://github.com/Mikdaam/Galaxy_Simulation",
+        image: galaxyPic,
+        categories: ["Langage C", "Algorithmes"],
+        title: "Galaxy Simulation",
+        description: "Simulation de mouvements de corps dans la galaxie, réalisé en C avec une librairie interne."
+    }
+];
+
+export const load = (async () => {
+    return {
+        projects,
+    };
+}) satisfies PageLoad;
