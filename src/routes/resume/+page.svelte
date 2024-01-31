8f68ee7bade852e33395c993cc689256e46ffb4c
Mick <bmikdaam@gmail.com>
2024-01-31 19:56:57 +0100

    Add resume page with formations and experiences

---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  import { BookOutline } from "svelte-ionicons";
+import type { PageData } from "./$types";
+
+  export let data: PageData;
+</script>
+
+<article class="active" data-page="resume">
+  <header>
+    <h2 class="h2 article-title">Parcours</h2>
+  </header>
+
+  <section class="timeline">
+    <div class="title-wrapper">
+      <div class="icon-box">
+        <BookOutline />
+      </div>
+      <h3 class="h3">Formations</h3>
+    </div>
+
+    <ol class="timeline-list">
+      {#each data.resume.formations as { title, date, details }}
+        <li class="timeline-item">
+          <h4 class="h4 timeline-item-title">{title}</h4>
+          <span>{date}</span>
+          <p class="timeline-text">{@html details}</p>
+        </li>
+      {/each}
+    </ol>
+  </section>
+
+  <section class="timeline">
+    <div class="title-wrapper">
+      <div class="icon-box">
+        <BookOutline />
+      </div>
+      <h3 class="h3">Expériences</h3>
+    </div>
+
+    <ol class="timeline-list">
+      {#each data.resume.experiences as { title, date, details }}
+        <li class="timeline-item">
+          <h4 class="h4 timeline-item-title">{title}</h4>
+          <span>{date}</span>
+          <p class="timeline-text">{@html details}</p>
+        </li>
+      {/each}
+    </ol>
+  </section>
+</article>
---
+++ src/routes/resume/+page.ts
@@ -0,0 +1,34 @@
+import type { PageLoad } from "./$types";
+
+const resume = {
+    formations: [
+        {
+            title: 'Diplome d\'Ingénieur Informatique | ESIEE Paris',
+            date: '2021 — 2024',
+            details: '• Informatique, design, architecture et développement - Option Réseaux <br />• Echange universitaire au Québec pendant un trimestre <br />• Last Project : Projet de fin d’étude',
+        },
+        {
+            title: 'DUT Informatique | IUT Sénart Fontainebleau',
+            date: '2019 — 2021',
+            details: '• Bases de la programmation informatique et des réseaux informatiques <br />• Réalisation d’un stage de fin d’étude : automatisations des tests d’applications',
+        },
+    ],
+    experiences: [
+        {
+            title: 'Alternant Développeur Supply Chain | Safran Aircraft Engines',
+            date: '2021 — Present',
+            details: '• Gestion de projet d’une application web.<br />• Utilisation avancée des technologies JavaScript (ReactJs, NodeJs) et SQL.<br />• Analyse de données avec Talend et Power BI.<br />• Création d\'indicateurs clés de performance (KPI) pour faciliter la migration vers SAP.',
+        },
+        {
+            title: 'Stagiaire Engine Factory | EasyVista',
+            date: 'Mars - Juin 2021',
+            details: 'Développement de scripts d\'automatisation pour les tests d\'applications web.<br />• Optimisation des scripts de tests existants.<br />• Utilisation du logiciel Ranorex Studio pour l\'automatisation des tests.',
+        },
+    ],
+};
+
+export const load: PageLoad = () => {
+    return {
+        resume,
+    };
+};
