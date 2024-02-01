e8d1967a795a4284fe610aa1c204b90b8aaa8e63
Mick <bmikdaam@gmail.com>
2024-02-01 10:51:16 +0100

    Updates

---
+++ _app/immutable/entry/start.PSDrcSof.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.PTd6LX-I.js";export{t as start};
---
+++ _app/immutable/nodes/2.b7lVKHjU.js
@@ -0,0 +1,8 @@
+import{s as r,n as a}from"../chunks/scheduler.TwWJsNmc.js";import{S as l,i as u,e as c,c as p,k as d,l as s,g as m,d as v}from"../chunks/index.3Tpho3Eu.js";function h(i){let e,n=`<header><h2 class="h2 article-title">A Propos</h2></header> <section class="about-text"><p>Je suis actuellement étudiant en dernière année de cursus ingénieur,
+      passionné par la technologie et désireux de mettre mes compétences en
+      ingénierie logicielle au service d&#39;une entreprise dynamique.</p> <p>Mon parcours académique m&#39;a permis de développer une expertise approfondie
+      en Java, tout en cultivant une approche polyvalente du développement en
+      explorant des technologies variées telles que Svelte, Node.js, Flutter,
+      Jetpack Compose, Java et Golang. Convaincu que la curiosité et
+      l&#39;adaptabilité sont essentielles dans un domaine en constante évolution,
+      je suis prêt à relever de nouveaux défis.</p></section>`;return{c(){e=c("article"),e.innerHTML=n,this.h()},l(t){e=p(t,"ARTICLE",{class:!0,"data-page":!0,"data-svelte-h":!0}),d(e)!=="svelte-19fu0n7"&&(e.innerHTML=n),this.h()},h(){s(e,"class","active"),s(e,"data-page","about")},m(t,o){m(t,e,o)},p:a,i:a,o:a,d(t){t&&v(e)}}}class x extends l{constructor(e){super(),u(this,e,null,h,r,{})}}export{x as component};
